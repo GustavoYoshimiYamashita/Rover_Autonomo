e51304a1212986b306739043fdc5a3be3d00f1b7
GustavoYoshimiYamashita <gustavo.yamashita@outlook.com>
2022-05-01 15:52:47 -0300

    Transformando o Lidar em 3D com 10 layers.

--- lidar_funcoes.py
+++ lidar_funcoes.py
@@ -1,4 +1,5 @@
 import math
+import pygame
 
 grafico = []
 momento = []
@@ -10,28 +11,35 @@ list_leste = []
 list_oeste = []
 graus = 0.0
 
+# Inicializando cor
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+black = (0, 0, 0)
+gray = (200, 200, 200)
+white = (255, 255, 255)
 vertical = 600
 horizontal = 600
-centroY = int(vertical / 2)
-centroX = int(horizontal / 2)
+centroY = int(vertical/2)
+centroX = int(horizontal/2)
 
+def leitura_lidar(number_points, Lidar, lidar, imagem):
+    grafico = []
+    momento = []
+    valor = []
+    list_valorX = []
+    list_valorY = []
+    list_norte = []
+    list_leste = []
+    list_oeste = []
+    graus = 0.0
 
-
-def leitura(lidar, number_points):
-    graus = 0
     for x in range(number_points):
-
-        # Coletando dado polar
-        imagem = lidar.getRangeImage()
-        # if imagem[x] != float('inf'):
-        #    print(imagem[x])
-
         valor.append(imagem[x])
         momento.append(x)
         grafico = [valor, momento]
         graus = graus + 2.8125
         rad = (graus * math.pi) / 180
-
 
         # Transformando em coordenada cartesiana
         # cosseno -> x
@@ -42,6 +50,46 @@ def leitura(lidar, number_points):
         retaY = math.sin(rad) * imagem[x]
         retaY = (retaY * 60) + centroY
         list_valorY.append(retaY)
-
+        # print(f"RetaX: {retaY}, RetaY: {retaY}")
     return list_valorX, list_valorY
 
+
+
+def desenhar_pontos_2d(Lidar, lidar, surface):
+    number_points = Lidar.getNumberOfPoints(lidar)
+    grafico = []
+    momento = []
+    valor = []
+    list_valorX = []
+    list_valorY = []
+    list_norte = []
+    list_leste = []
+    list_oeste = []
+    graus = 0.0
+
+    # Coletando dado polar
+    imagem = lidar.getRangeImage()
+
+    for x in range(number_points):
+        valor.append(imagem[x])
+        momento.append(x)
+        grafico = [valor, momento]
+        graus = graus + 2.8125
+        rad = (graus * math.pi) / 180
+
+        # Transformando em coordenada cartesiana
+        # cosseno -> x
+        # seno -> y
+        retaX = math.cos(rad) * imagem[x]
+        retaX = (retaX * 60) + centroX
+        list_valorX.append(retaX)
+        retaY = math.sin(rad) * imagem[x]
+        retaY = (retaY * 60) + centroY
+        list_valorY.append(retaY)
+        # print(f"RetaX: {retaY}, RetaY: {retaY}")
+
+        # line(surface, color, start_pos, end_pos)
+        if valor[x] != float('inf'):
+            pygame.draw.line(surface, blue, (centroX, centroY), (list_valorX[x], list_valorY[x]))
+
+        pygame.draw.circle(surface, white, (list_valorX[x], list_valorY[x]), 5)
--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from controller import *
 import movimentacao
 import math
 import pygame
+import lidar_funcoes
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+import numpy as np
+from datetime import datetime
 
 TIME_STEP = 10
 
@@ -44,6 +49,10 @@ compass.enable(TIME_STEP)
 direction = 0
 initial_value = True
 
+# Adicionando camera
+camera = robot.getDevice('camera')
+camera.enable(TIME_STEP)
+
 # Inicializando cor
 red = (255, 0, 0)
 green = (0, 255, 0)
@@ -58,8 +67,13 @@ centroX = int(horizontal/2)
 
 surface = pygame.display.set_mode((horizontal, vertical))
 
+now = datetime.now()
+
+
 while robot.step(TIME_STEP) != -1:
-    movimentacao.set_speed(0.6, motores_velocidade)
+
+
+    movimentacao.set_speed(0, motores_velocidade)
 
     for event in pygame.event.get():
         if event.type == 256:
@@ -68,42 +82,57 @@ while robot.step(TIME_STEP) != -1:
 
     surface.fill(black)
     pygame.draw.circle(surface, white, (centroX, centroY), 5)
-    number_points = Lidar.getNumberOfPoints(lidar)
-    grafico = []
-    momento = []
-    valor = []
-    list_valorX = []
-    list_valorY = []
-    list_norte = []
-    list_leste =[]
-    list_oeste = []
-    graus = 0.0
 
-    for x in range(number_points):
+    # Coletando dado polar
+    imagem = lidar.getRangeImage()
 
-        #Coletando dado polar
-        imagem = lidar.getRangeImage()
-        valor.append(imagem[x])
-        momento.append(x)
-        grafico = [valor, momento]
-        graus = graus + 2.8125
-        rad = (graus * math.pi) / 180
+    list = []
+    range1 = lidar.getLayerRangeImage(0)
+    range2 = lidar.getLayerRangeImage(1)
+    range3 = lidar.getLayerRangeImage(2)
+    range4 = lidar.getLayerRangeImage(3)
+    range5 = lidar.getLayerRangeImage(4)
+    range6 = lidar.getLayerRangeImage(5)
+    range7 = lidar.getLayerRangeImage(6)
+    range8 = lidar.getLayerRangeImage(7)
+    range9 = lidar.getLayerRangeImage(8)
+    range10 = lidar.getLayerRangeImage(9)
 
-        # Transformando em coordenada cartesiana
-        # cosseno -> x
-        # seno -> y
-        retaX = math.cos(rad) * imagem[x]
-        retaX = (retaX * 60) + centroX
-        list_valorX.append(retaX)
-        retaY = math.sin(rad) * imagem[x]
-        retaY = (retaY * 60) + centroY
-        list_valorY.append(retaY)
-        #print(f"RetaX: {retaY}, RetaY: {retaY}")
+    numberRange1 = len(range1)
+    numberRange2 = len(range2)
+    numberRange3 = len(range3)
+    numberRange4 = len(range4)
+    numberRange5 = len(range5)
+    numberRange6 = len(range6)
+    numberRange7 = len(range7)
+    numberRange8 = len(range8)
+    numberRange9 = len(range9)
+    numberRange10 = len(range10)
 
-        # line(surface, color, start_pos, end_pos)
-        if valor[x] != float('inf'):
-            pygame.draw.line(surface, blue, (centroX, centroY), (list_valorX[x], list_valorY[x]))
+    listaValorXRange1, listaValorYRange1 = lidar_funcoes.leitura_lidar(numberRange1, Lidar, lidar, range1)
+    listaValorXRange2, listaValorYRange2 = lidar_funcoes.leitura_lidar(numberRange2, Lidar, lidar, range2)
+    listaValorXRange3, listaValorYRange3 = lidar_funcoes.leitura_lidar(numberRange3, Lidar, lidar, range3)
+    listaValorXRange4, listaValorYRange4 = lidar_funcoes.leitura_lidar(numberRange4, Lidar, lidar, range4)
+    listaValorXRange5, listaValorYRange5 = lidar_funcoes.leitura_lidar(numberRange5, Lidar, lidar, range5)
+    listaValorXRange6, listaValorYRange6 = lidar_funcoes.leitura_lidar(numberRange6, Lidar, lidar, range6)
+    listaValorXRange7, listaValorYRange7 = lidar_funcoes.leitura_lidar(numberRange7, Lidar, lidar, range7)
+    listaValorXRange8, listaValorYRange8 = lidar_funcoes.leitura_lidar(numberRange8, Lidar, lidar, range8)
+    listaValorXRange9, listaValorYRange9 = lidar_funcoes.leitura_lidar(numberRange9, Lidar, lidar, range9)
+    listaValorXRange10, listaValorYRange10 = lidar_funcoes.leitura_lidar(numberRange10, Lidar, lidar, range10)
 
-        pygame.draw.circle(surface, white, (list_valorX[x], list_valorY[x]), 5)
+    ax = plt.axes(projection='3d')
+
+    # Data for three-dimensional scattered points
+    zdata = 128 * [1] + 128 * [2] + 128 * [3] + 128 * [4] + 128 * [5] + 128 * [6] + 128 * [7] + 128 * [8] + 128 * [9] + 128 * [10]
+    xdata = listaValorXRange1 + listaValorXRange2 + listaValorXRange3 + listaValorXRange4 + listaValorXRange5 + listaValorXRange6 + listaValorXRange7 + listaValorXRange8 + listaValorXRange9 + listaValorXRange10
+    ydata = listaValorYRange1 + listaValorYRange2 + listaValorYRange3 + listaValorYRange4 + listaValorYRange5 + listaValorYRange6 + listaValorYRange7 + listaValorYRange8 + listaValorYRange9 + listaValorYRange10
+    ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens')
+
+    if (datetime.now() - now).seconds > 5:
+        now = datetime.now()
+        plt.show()
+
     pygame.display.update()
     pygame.display.flip()
+
+
